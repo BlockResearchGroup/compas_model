0f6d5cc19eebeb09b790e02968aaeb7b11dd3014
tomvanmele <tom.v.mele@gmail.com>
2024-02-22 08:29:29 +0100

    add interface

--- src/compas_model/elements/__init__.py
+++ src/compas_model/elements/__init__.py
@@ -1,10 +1,12 @@
 from .element import Element
 from .element import Feature
 from .block import BlockElement
+from .interface import InterfaceElement
 
 
 __all__ = [
     "Element",
     "Feature",
     "BlockElement",
+    "InterfaceElement",
 ]
