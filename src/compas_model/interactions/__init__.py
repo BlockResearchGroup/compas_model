e73a4ea79357388ec708957db5c031f7eb7256d1
tomvanmele <tom.v.mele@gmail.com>
2024-02-24 12:07:27 +0100

    description of the meaning of "interaction"

--- src/compas_model/interactions/__init__.py
+++ src/compas_model/interactions/__init__.py
@@ -1,3 +1,9 @@
+"""This module provides classes for defining the type of interaction that exists between two elements.
+The interaction type could determine, for example, how forces are transferred from one element to the other.
+The interaction type could also determine whether an interaction is permanent or temporary; for example, for designing construction sequences.
+The different types of interactions will have to be interpreted by the context in which the model is used.
+"""
+
 from .interaction import Interaction
 
 __all__ = [
