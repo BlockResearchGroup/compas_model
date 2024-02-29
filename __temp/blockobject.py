7b7f5eeb181f0e8092cdf6ffaffadc51d6fa8301
tomvanmele <tom.v.mele@gmail.com>
2024-02-29 18:38:54 +0100

    move to real temp

--- __temp/blockobject.py
+++
@@ -1,7 +0,0 @@
-from compas.scene import SceneObject
-
-
-class BlockObject(SceneObject):
-    def __init__(self, element, **kwargs):
-        super(BlockObject, self).__init__(item=element, **kwargs)
-        self.element = element
