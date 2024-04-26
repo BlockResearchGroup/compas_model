47a575e95e2ddf25a5d410d98199aa7936f34800
Chen Kasirer <ckasirer@ethz.ch>
2024-04-26 14:35:01 +0200

    str() instead of formating functions

--- src/compas_model/model/model.py
+++ src/compas_model/model/model.py
@@ -116,11 +116,11 @@ class Model(Datastructure):
         output = "=" * 80 + "\n"
         output += "Spatial Hierarchy\n"
         output += "=" * 80 + "\n"
-        output += self._tree.hierarchy()
+        output += str(self._tree)
         output += "=" * 80 + "\n"
         output += "Element Interactions\n"
         output += "=" * 80 + "\n"
-        output += self._graph.interactions()
+        output += str(self._graph)
         output += "=" * 80 + "\n"
         output += "Element Groups\n"
         output += "=" * 80 + "\n"
