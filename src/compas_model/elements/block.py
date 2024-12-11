20b1aad3fa3f39e5019139f2eca6e7497186b690
tomvanmele <tom.v.mele@gmail.com>
2024-12-11 19:43:27 +0100

    block annotations

--- src/compas_model/elements/block.py
+++ src/compas_model/elements/block.py
@@ -1,8 +1,11 @@
-import compas.datastructures  # noqa: F401
+from typing import Optional
+from typing import Union
+
 from compas.datastructures import Mesh
 from compas.geometry import Box
 from compas.geometry import Frame
 from compas.geometry import Point
+from compas.geometry import Transformation
 from compas.geometry import bestfit_frame_numpy
 from compas.geometry import bounding_box
 from compas.geometry import centroid_points
@@ -25,7 +28,67 @@ Frame.invert = invert
 
 
 class BlockGeometry(Mesh):
-    def face_frame(self, face):
+    """Geometric representation of a block using a mesh."""
+
+    @property
+    def top(self) -> int:
+        """Identify the *top* face of the block.
+
+        Returns
+        -------
+        int
+            The identifier of the face.
+
+        """
+        z = [0, 0, 1]
+        faces = list(self.faces())
+        normals = [self.face_normal(face) for face in faces]
+        return sorted(zip(faces, normals), key=lambda x: dot_vectors(x[1], z))[-1][0]
+
+    @property
+    def centroid(self) -> Point:
+        """Compute the centroid of the block.
+
+        Returns
+        -------
+        :class:`compas.geometry.Point`
+
+        """
+        x, y, z = centroid_points([self.vertex_coordinates(key) for key in self.vertices()])
+        return Point(x, y, z)
+
+    @property
+    def center(self) -> Point:
+        """Compute the center of mass of the block.
+
+        Returns
+        -------
+        :class:`compas.geometry.Point`
+
+        """
+        vertex_index = {vertex: index for index, vertex in enumerate(self.vertices())}
+        vertices = [self.vertex_coordinates(vertex) for vertex in self.vertices()]
+        faces = [[vertex_index[vertex] for vertex in self.face_vertices(face)] for face in self.faces()]
+        x, y, z = centroid_polyhedron((vertices, faces))
+        return Point(x, y, z)
+
+    @property
+    def volume(self) -> float:
+        """Compute the volume of the block.
+
+        Returns
+        -------
+        float
+            The volume of the block.
+
+        """
+        vertex_index = {vertex: index for index, vertex in enumerate(self.vertices())}
+        vertices = [self.vertex_coordinates(vertex) for vertex in self.vertices()]
+        faces = [[vertex_index[vertex] for vertex in self.face_vertices(face)] for face in self.faces()]
+        v = volume_polyhedron((vertices, faces))
+        return v
+
+    def face_frame(self, face: int) -> Frame:
         """Compute the frame of a specific face.
 
         Parameters
@@ -45,60 +108,6 @@ class BlockGeometry(Mesh):
         if frame.zaxis.dot(normal) < 0:
             frame.invert()
         return frame
-
-    def top(self):
-        """Identify the *top* face of the block.
-
-        Returns
-        -------
-        int
-            The identifier of the face.
-
-        """
-        z = [0, 0, 1]
-        faces = list(self.faces())
-        normals = [self.face_normal(face) for face in faces]
-        return sorted(zip(faces, normals), key=lambda x: dot_vectors(x[1], z))[-1][0]
-
-    def centroid(self):
-        """Compute the centroid of the block.
-
-        Returns
-        -------
-        :class:`compas.geometry.Point`
-
-        """
-        x, y, z = centroid_points([self.vertex_coordinates(key) for key in self.vertices()])
-        return Point(x, y, z)
-
-    def center(self):
-        """Compute the center of mass of the block.
-
-        Returns
-        -------
-        :class:`compas.geometry.Point`
-
-        """
-        vertex_index = {vertex: index for index, vertex in enumerate(self.vertices())}
-        vertices = [self.vertex_coordinates(vertex) for vertex in self.vertices()]
-        faces = [[vertex_index[vertex] for vertex in self.face_vertices(face)] for face in self.faces()]
-        x, y, z = centroid_polyhedron((vertices, faces))
-        return Point(x, y, z)
-
-    def volume(self):
-        """Compute the volume of the block.
-
-        Returns
-        -------
-        float
-            The volume of the block.
-
-        """
-        vertex_index = {vertex: index for index, vertex in enumerate(self.vertices())}
-        vertices = [self.vertex_coordinates(vertex) for vertex in self.vertices()]
-        faces = [[vertex_index[vertex] for vertex in self.face_vertices(face)] for face in self.faces()]
-        v = volume_polyhedron((vertices, faces))
-        return v
 
 
 # A block could have features like notches,
@@ -148,24 +157,30 @@ class BlockElement(Element):
         data["is_support"] = self.is_support
         return data
 
-    def __init__(self, shape, features=None, is_support=False, frame=None, transformation=None, name=None):
-        # type: (Mesh | BlockGeometry, list[BlockFeature] | None, bool, compas.geometry.Frame | None, compas.geometry.Transformation | None, str | None) -> None
-        super().__init__(frame=frame, transformation=transformation, name=name)
+    def __init__(
+        self,
+        shape: Union[Mesh, BlockGeometry],
+        features: Optional[list[BlockFeature]] = None,
+        is_support: bool = False,
+        frame: Optional[Frame] = None,
+        transformation: Optional[Transformation] = None,
+        name: Optional[str] = None,
+    ) -> None:
+        super().__init__(frame=frame, transformation=transformation, features=features, name=name)
 
         self.shape = shape if isinstance(shape, BlockGeometry) else shape.copy(cls=BlockGeometry)
-        self.features = features or []  # type: list[BlockFeature]
         self.is_support = is_support
 
     # =============================================================================
     # Implementations of abstract methods
     # =============================================================================
 
-    def compute_elementgeometry(self) -> Mesh | Brep:
+    def compute_elementgeometry(self) -> Union[Mesh, Brep]:
         geometry = self.shape
         # apply features?
         return geometry
 
-    def compute_modelgeometry(self) -> Mesh | Brep:
+    def compute_modelgeometry(self) -> Union[Mesh, Brep]:
         if not self.model:
             raise Exception
 
@@ -188,23 +203,23 @@ class BlockElement(Element):
 
         return geometry
 
-    def compute_aabb(self, inflate=0.0):
-        points = self.modelgeometry.vertices_attributes("xyz")  # type: ignore
+    def compute_aabb(self) -> Box:
+        points = self.modelgeometry.vertices_attributes("xyz")
         box = Box.from_bounding_box(bounding_box(points))
-        box.xsize += inflate
-        box.ysize += inflate
-        box.zsize += inflate
+        box.xsize += self.inflate_aabb
+        box.ysize += self.inflate_aabb
+        box.zsize += self.inflate_aabb
         return box
 
-    def compute_obb(self, inflate=0.0):
-        points = self.modelgeometry.vertices_attributes("xyz")  # type: ignore
+    def compute_obb(self) -> Box:
+        points = self.modelgeometry.vertices_attributes("xyz")
         box = Box.from_bounding_box(oriented_bounding_box(points))
-        box.xsize += inflate
-        box.ysize += inflate
-        box.zsize += inflate
+        box.xsize += self.inflate_obb
+        box.ysize += self.inflate_obb
+        box.zsize += self.inflate_obb
         return box
 
-    def compute_collision_mesh(self):
+    def compute_collision_mesh(self) -> Mesh:
         # TODO: (TvM) make this a pluggable with default implementation in core and move import to top
         from compas.geometry import convex_hull_numpy
 
@@ -218,8 +233,7 @@ class BlockElement(Element):
     # =============================================================================
 
     @classmethod
-    def from_box(cls, box):
-        # type: (compas.geometry.Box) -> BlockElement
+    def from_box(cls, box: Box) -> "BlockElement":
         shape = box.to_mesh()
         block = cls(shape=shape)
         return block
