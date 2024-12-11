3c69e8ea7525bb341924f3c790669023b67703c8
petras <petrasvestartas@gmail.com>
2024-12-11 18:42:00 +0100

    ADD grid_model example empty

---
+++ docs/examples/grid_model/000_one_unit.py
@@ -0,0 +1,5 @@
+# Example file to do the following:
+# Create a model with 4 columns, 4 column-heads and 1 plate.
+# Compute interactions between the elements. TODO: Add interactions and the base element level when geometry is computed.
+# Change one column head and check if the elements are dirty.
+# Recompute the modelgeometry.
