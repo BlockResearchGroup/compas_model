c1452b60de259c5e6123cd3ba0b2e37bfc445f28
tomvanmele <tom.v.mele@gmail.com>
2024-12-11 21:17:40 +0100

    type annotations materials

--- src/compas_model/materials/timber.py
+++ src/compas_model/materials/timber.py
@@ -1,13 +1,13 @@
+from typing import Optional
+
 from .material import Material
 
 
 class Timber(Material):
     @property
-    def __data__(self):
-        # type: () -> dict
-        data = super(Timber, self).__data__
-        data.update({})
+    def __data__(self) -> dict:
+        data = super().__data__
         return data
 
-    def __init__(self, name=None):
-        super(Timber, self).__init__(name=name)
+    def __init__(self, name: Optional[str] = None) -> None:
+        super().__init__(name=name)
