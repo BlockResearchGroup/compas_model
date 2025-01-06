0546f87822b71a5d8cc67867a36839c2b7041e70
petras <petrasvestartas@gmail.com>
2025-01-06 14:36:53 +0100

    ADD BooleanModifier and SlicerModifier imports to __init__.

--- src/compas_model/interactions/__init__.py
+++ src/compas_model/interactions/__init__.py
@@ -2,8 +2,12 @@ from .interaction import Interaction
 from .contact import (
     ContactInterface,
 )
+from .boolean_modifier import BooleanModifier
+from .slicer_modifier import SlicerModifier
 
 __all__ = [
     "Interaction",
     "ContactInterface",
+    "BooleanModifier",
+    "SlicerModifier",
 ]
