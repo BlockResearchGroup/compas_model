48bda163925bdfa61a220da795473a89a492253b
petras <petrasvestartas@gmail.com>
2025-01-06 14:33:32 +0100

    ADD BooleanModifier class with modify method.

---
+++ src/compas_model/interactions/boolean_modifier.py
@@ -0,0 +1,40 @@
+from compas.geometry import Plane  # noqa: F401
+
+from .interaction import Interaction
+
+
+class BooleanModifier(Interaction):
+    """Perform boolean difference on the target element.
+
+    Parameters
+    ----------
+    name : str, optional
+        The name of the interaction.
+
+    """
+
+    @property
+    def __data__(self):
+        # type: () -> dict
+        return {"name": self.name}
+
+    def __init__(self, name=None):
+        # type: (str | None) -> None
+        super(BooleanModifier, self).__init__(name=name)
+
+    def __repr__(self):
+        return '{}(name="{}")'.format(self.__class__.__name__, self.name)
+
+    def modify(self, targetgeometry, _, sourceelement):
+        """Apply the interaction to the affected geometry.
+
+        Parameters
+        ----------
+        targetgeometry : Brep or Mesh
+            The geometry to be affected iteratively. The same geometry can be modified multiple times.
+        sourceelement : Element
+            The element that is transformed to the modelgeometry frame.
+        """
+        from compas_model.algorithms.modifiers import boolean_difference  # Local import is needed otherwise, remove contact interactions in algorithms module.
+
+        return boolean_difference(targetgeometry, sourceelement.modelgeometry)
