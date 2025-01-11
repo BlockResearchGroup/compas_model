3807b583c5ca9e4ffeec929bfa509c6edba61210
tomvanmele <tom.v.mele@gmail.com>
2025-01-11 14:28:50 +0100

    remove old collisions for now

--- src/compas_model/algorithms/__init__.py
+++ src/compas_model/algorithms/__init__.py
@@ -1,8 +1,3 @@
-from .collisions import is_aabb_aabb_collision
-from .collisions import is_box_box_collision
-from .collisions import is_face_to_face_collision
-
-from .collisions import get_collision_pairs  # rename to model_collisions
 from .interfaces import model_interfaces
 from .intersections import model_intersections
 from .overlaps import model_overlaps
@@ -11,10 +6,6 @@ from .modifiers import boolean_difference
 
 
 __all__ = [
-    "is_aabb_aabb_collision",
-    "is_box_box_collision",
-    "is_face_to_face_collision",
-    "get_collision_pairs",
     "model_interfaces",
     "model_intersections",
     "model_overlaps",
