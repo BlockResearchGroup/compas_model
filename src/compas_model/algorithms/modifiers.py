5895c7819ecccb9da027c8fbe7fbad39504da63e
petras <petrasvestartas@gmail.com>
2025-01-15 15:58:28 +0100

    FIX missing return statement in slice method.

--- src/compas_model/algorithms/modifiers.py
+++ src/compas_model/algorithms/modifiers.py
@@ -23,21 +23,18 @@ def slice(geometry: Union[Brep, Mesh], slice_plane: Plane) -> Union[Brep, Mesh]:
         The sliced geometry.
     """
 
-    if isinstance(geometry, Brep):
-        try:
+    try:
+        if isinstance(geometry, Brep):
             geometry.make_solid()
             slice_plane_flipped = Plane(slice_plane.point, -slice_plane.normal)
             geometry.trim(slice_plane_flipped)
-        except Exception:
-            print("SlicerModifier is not successful.")
             return geometry
-    else:
-        try:
+        else:
             split_meshes: Optional[list] = geometry.slice(slice_plane)
             return split_meshes[0] if split_meshes else geometry
-        except Exception:
-            print("SlicerModifier is not successful.")
-            return geometry
+    except Exception:
+        print("SlicerModifier is not successful.")
+        return geometry
 
 
 def boolean_difference(target_geometry, source_geometry):
@@ -57,24 +54,19 @@ def boolean_difference(target_geometry, source_geometry):
         The geometry after boolean difference.
     """
 
-    is_brep0: bool = isinstance(target_geometry, Brep)
-    is_brep1: bool = isinstance(source_geometry, Brep)
-
-    if is_brep0 and is_brep1:
+    if isinstance(target_geometry, Brep) and isinstance(source_geometry, Brep):
         try:
             return Brep.from_boolean_difference(target_geometry, source_geometry)
         except Exception:
             print("Boolean difference is not successful.")
             return target_geometry
-
     else:
         from compas_cgal.booleans import boolean_difference_mesh_mesh
 
-        mesh0: Mesh = target_geometry.copy() if not is_brep0 else Mesh.from_polygons(target_geometry.to_polygons())
-        mesh1: Mesh = source_geometry.copy() if not is_brep1 else Mesh.from_polygons(source_geometry.to_polygons())
+        mesh0: Mesh = target_geometry.copy() if not isinstance(target_geometry, Brep) else Mesh.from_polygons(target_geometry.to_polygons())
+        mesh1: Mesh = source_geometry.copy() if not isinstance(source_geometry, Brep) else Mesh.from_polygons(source_geometry.to_polygons())
 
         A = mesh0.to_vertices_and_faces(triangulated=True)
         B = mesh1.to_vertices_and_faces(triangulated=True)
         V, F = boolean_difference_mesh_mesh(A, B)
-        mesh: Mesh = Mesh.from_vertices_and_faces(V, F) if len(V) > 0 and len(F) > 0 else mesh0
-        return mesh
+        return Mesh.from_vertices_and_faces(V, F) if V and F else mesh0
