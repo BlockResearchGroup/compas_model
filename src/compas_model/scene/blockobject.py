211738a1104690d4f77da883b078634b2d1ccfc1
tomvanmele <tom.v.mele@gmail.com>
2025-01-16 16:31:56 +0100

    remove block object

--- src/compas_model/scene/blockobject.py
+++
@@ -1,8 +0,0 @@
-from compas_model.scene import ElementObject
-
-
-class BlockObject(ElementObject):
-    """Scene object for drawing a block."""
-
-    def __init__(self, block, **kwargs):
-        super().__init__(element=block, **kwargs)
