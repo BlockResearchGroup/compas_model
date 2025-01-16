a6c7cb87f28f52e48d0ac315bbd55251538a5cb7
tomvanmele <tom.v.mele@gmail.com>
2025-01-16 20:57:44 +0100

    clean up scene imports

--- src/compas_model/notebook/scene/__init__.py
+++ src/compas_model/notebook/scene/__init__.py
@@ -1,20 +1,18 @@
-"""This package provides scene object plugins for visualising COMPAS Model objects in Rhino.
-When working in Rhino, :class:`compas.scene.SceneObject` will automatically use
-the corresponding Rhino scene object for each COMPAS model object type.
-
-"""
-
 from compas.plugins import plugin
 from compas.scene import register
 
-from compas_model.elements import BlockElement
 from compas_model.models import Model
 
-from .blockobject import ThreeBlockObject
+from .elementobject import ThreeElementObject
 from .modelobject import ThreeModelObject
 
 
 @plugin(category="factories", requires=["pythreejs"])
 def register_scene_objects():
-    register(BlockElement, ThreeBlockObject, context="Notebook")
     register(Model, ThreeModelObject, context="Notebook")
+
+
+__all__ = [
+    "ThreeElementObject",
+    "ThreeModelObject",
+]
