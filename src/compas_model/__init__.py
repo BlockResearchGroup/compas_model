8581e73a0e284490c42d01a18eaf609e141a36a0
tomvanmele <tom.v.mele@gmail.com>
2025-01-21 15:13:34 +0100

    Bump version to 0.5.0

--- src/compas_model/__init__.py
+++ src/compas_model/__init__.py
@@ -4,7 +4,7 @@ __author__ = ["petras vestartas"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "petrasvestartas@gmail.com"
-__version__ = "0.4.5"
+__version__ = "0.5.0"
 
 HERE = os.path.dirname(__file__)
 
