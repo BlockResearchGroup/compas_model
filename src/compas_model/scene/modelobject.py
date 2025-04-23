a969100055edf2ce69fee4703bfa3df4cd0334eb
Licini <leochen1990@gmail.com>
2025-04-23 10:35:02 +0200

    simplify model object

--- src/compas_model/scene/modelobject.py
+++ src/compas_model/scene/modelobject.py
@@ -1,40 +1,16 @@
-from typing import Optional
-
 from compas.geometry import Transformation
-from compas.scene import Group
 from compas.scene import SceneObject
 from compas_model.models import Model
 
 
 class ModelObject(SceneObject):
-    def __init__(
-        self,
-        show_elements: Optional[bool] = True,
-        show_contacts: Optional[bool] = False,
-        **kwargs,
-    ) -> None:
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-        self.show_elements = show_elements
-        self.show_contacts = show_contacts
-
-        self._add_elements(**kwargs)
-
-    def _add_elements(self, **kwargs) -> None:
-        elements_group = self.add(Group(name="Elements", context=self.context))
-        contacts_group = self.add(Group(name="Contacts", context=self.context))
-
-        if self.show_elements:
-            for element in self.model.tree.rootelements:
-                element_kwargs = kwargs.copy()
-                element_kwargs["item"] = element
-                elements_group.add(**element_kwargs)
-
-        if self.show_contacts:
-            for contact in self.model.contacts():
-                contact_kwargs = kwargs.copy()
-                contact_kwargs["item"] = contact
-                contacts_group.add(**contact_kwargs)
+        for element in self.model.tree.rootelements:
+            element_kwargs = kwargs.copy()
+            element_kwargs["item"] = element
+            self.add(**element_kwargs)
 
     @property
     def model(self) -> Model:
