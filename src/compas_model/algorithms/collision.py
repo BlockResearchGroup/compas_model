cec77bbf2336761d7a173c674180f93d6c1b9606
tomvanmele <tom.v.mele@gmail.com>
2025-05-12 21:55:34 +0200

    collision stubs as booleans

--- src/compas_model/algorithms/collision.py
+++ src/compas_model/algorithms/collision.py
@@ -1,14 +1,14 @@
-def box_box_collision(A, B):
+def is_box_box_collision(A, B):
     raise NotImplementedError
 
 
-def polyhedron_polyhedron_collision(A, B):
+def is_polyhedron_polyhedron_collision(A, B):
     raise NotImplementedError
 
 
-def mesh_mesh_collision(A, B):
+def is_mesh_mesh_collision(A, B):
     raise NotImplementedError
 
 
-def brep_brep_collision(A, B):
+def is_brep_brep_collision(A, B):
     raise NotImplementedError
