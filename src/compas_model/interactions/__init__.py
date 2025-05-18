eb91b214cad7bd267d0b6acb8700cae7d7f7ed4e
tomvanmele <tom.v.mele@gmail.com>
2025-05-18 18:14:26 +0200

    removed modifiers from interactions

--- src/compas_model/interactions/__init__.py
+++ src/compas_model/interactions/__init__.py
@@ -1,11 +1,5 @@
 from .contact import Contact
-from .modifiers.modifier import Modifier
-from .modifiers.slicer_modifier import SlicerModifier
-from .modifiers.boolean_modifier import BooleanModifier
 
 __all__ = [
     "Contact",
-    "Modifier",
-    "SlicerModifier",
-    "BooleanModifier",
 ]
